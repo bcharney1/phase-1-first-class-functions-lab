7662d01bdedfe37a9a28a3538c2eef1d12340969
Boris Charney <boris.charney@gmail.com>
2022-09-06 21:26:46 -0700

    commit tree e44a66f0f72620f685e0217fd30d69ee73e10d73

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    return [ drivers[0], drivers[1] ]
+}
+const returnLastTwoDrivers = (drivers) => {
+    const driverLen = drivers.length
+    return [ drivers[driverLen -2], drivers[driverLen -1] ]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (multiplier) => {
+    return (fare) => {return fare * multiplier}
+} 
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (drivers, listFunction) => {
+    return listFunction(drivers);
+}
